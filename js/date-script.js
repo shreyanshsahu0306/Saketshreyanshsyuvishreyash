27b3e521af1a354c6ece73a218677d5ff8533064
YuvrajRajpoot-Dev <rajpootyuvraj222@gmail.com>
2021-04-20 14:09:15 +0530

    remove top bar

--- js/date-script.js
+++
@@ -1,8 +0,0 @@
- $( function() {
-    $( "#datepicker, #weddingdate, #taskdate" ).datepicker({
-    	 
-
-    });
-    $( '#ui-datepicker-div' ).before('<div class="default-skin"></div>');
-    $( '#ui-datepicker-div' ).appendTo( '.default-skin' ).contents();
-  });
